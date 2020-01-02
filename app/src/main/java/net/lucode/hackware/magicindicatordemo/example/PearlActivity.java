15683c641a354673f7c439865d96b2885a67a619
euzl <euzl@khu.ac.kr>
2020-01-02 16:21:51 +0900

    magic indicator 설정
    아직 안뜬다 ㅠㅠㅠ
     뭐가문제지... 내 생각에 CHANNELS.length라고 해놨는데, CHANNELS가 화면에 뜨는 설정을 아직 안해줘서 그런 듯..?

--- app/src/main/java/net/lucode/hackware/magicindicatordemo/example/PearlActivity.java
+++ app/src/main/java/net/lucode/hackware/magicindicatordemo/example/PearlActivity.java
@@ -1,16 +1,30 @@
 package net.lucode.hackware.magicindicatordemo.example;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import net.lucode.hackware.magicindicator.MagicIndicator;
+import net.lucode.hackware.magicindicator.ViewPagerHelper;
+import net.lucode.hackware.magicindicator.buildins.circlenavigator.CircleNavigator;
 import net.lucode.hackware.magicindicatordemo.R;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+// Todo : Set Text message
+// Todo : Add Indicator
+// Todo : Make pretty
 public class PearlActivity extends AppCompatActivity {
 
+    private static final String[] CHANNELS = new String[]{"첫번째페이지", "두번째페이지", "세번째"};
     private ArrayList<Integer> imageList;
+    private List<String> textList = Arrays.asList(CHANNELS);
+
+    private ViewPager mViewPager;
+
     private static final int DP = 24;
 
     @Override
@@ -40,5 +54,21 @@ public class PearlActivity extends AppCompatActivity {
         imageList.add(R.drawable.image2);
         imageList.add(R.drawable.image3);
 
+    }
+
+    private void initMagicIndicator() {
+        MagicIndicator magicIndicator = (MagicIndicator) findViewById(R.id.magic_indicator);
+        CircleNavigator circlenavigator = new CircleNavigator(this);
+        circlenavigator.setCircleCount(CHANNELS.length);
+        circlenavigator.setFollowTouch(false);
+        circlenavigator.setCircleColor(Color.GREEN);
+        circlenavigator.setCircleClickListener(new CircleNavigator.OnCircleClickListener() {
+            @Override
+            public void onClick(int index) {
+                mViewPager.setCurrentItem(index);
+            }
+        });
+        magicIndicator.setNavigator(circlenavigator);
+        ViewPagerHelper.bind(magicIndicator, mViewPager);
     }
 }
